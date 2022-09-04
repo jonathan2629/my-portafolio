93a2d20f498d7c93fb85c6251a66b78638eaa7b3
JONATHAN CACERES <jonathancaceres2629@gmail.com>
2022-09-04 08:26:27 -0500

    desarrolando-portafolio

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle_button')[0]
+const navbarLinks = document.getElementsByClassName('navbar_links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
